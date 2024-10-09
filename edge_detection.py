9e8f792310382e62a9d42fea8c40c1fadf35fcdd
Andrey A. da Rosa <dey2010dey@gmail.com>
2024-10-09 18:18:47 -0300

    show images one by one

--- edge_detection.py
+++ edge_detection.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 import argparse
 
+def wait_key():
+    # Esperar por uma tecla ou fechar a janela após 30 segundos
+    if cv2.waitKey(30000) & 0xFF == ord('q'):
+        pass
+
 # Configurar o parser de argumentos
 parser = argparse.ArgumentParser(description="Detecção de linhas em uma imagem de tabuleiro de xadrez.")
 parser.add_argument("imagem_caminho", type=str, help="Caminho para a imagem do tabuleiro de xadrez.")
@@ -17,15 +22,35 @@ else:
     # Converter para escala de cinza
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    cv2.namedWindow("Gray", cv2.WINDOW_NORMAL)
+    cv2.imshow("Gray", gray)
+    wait_key()
+    cv2.destroyAllWindows()
+
     # Aplicar filtro Gaussian para suavizar a imagem e reduzir ruído
     blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+
+    cv2.namedWindow("Blurred", cv2.WINDOW_NORMAL)
+    cv2.imshow("Blurred", blurred)
+    wait_key()
+    cv2.destroyAllWindows()
 
     # Detectar as bordas usando Canny
     edges = cv2.Canny(blurred, 50, 150, apertureSize=3)
 
+    cv2.namedWindow("Edges", cv2.WINDOW_NORMAL)
+    cv2.imshow("Edges", edges)
+    wait_key()
+    cv2.destroyAllWindows()
+
     # Aplicar dilatação para fechar pequenas lacunas nas bordas
     kernel = np.ones((3, 3), np.uint8)
     dilated = cv2.dilate(edges, kernel, iterations=1)
+
+    cv2.namedWindow("Dilated", cv2.WINDOW_NORMAL)
+    cv2.imshow("Dilated", dilated)
+    wait_key()
+    cv2.destroyAllWindows
 
     # Aplicar Transformada de Hough para detectar as linhas
     lines = cv2.HoughLinesP(dilated, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=10)
@@ -40,10 +65,5 @@ else:
     # Exibir a imagem original com as linhas detectadas
     cv2.namedWindow("Linhas Detectadas", cv2.WINDOW_NORMAL)
     cv2.imshow("Linhas Detectadas", img)
-
-    # Esperar por uma tecla ou fechar a janela após 30 segundos
-    if cv2.waitKey(30000) & 0xFF == ord('q'):
-        pass
-
-    # Fechar todas as janelas
+    wait_key()
     cv2.destroyAllWindows()
